94a09171edde872709b4d7f10c49b9a8128c628f
Victor Lopez <vicalop@gmail.com>
2026-08-20 15:36:09 -0500

    Bump version to 0.3.0

--- js/src/index.js
+++ js/src/index.js
@@ -17,7 +17,7 @@ import { Toggle, ToggleGroup } from './toggle.js';
 import { Chart } from './chart.js';
 
 const bootcn = {
-  version: '0.2.3',
+  version: '0.3.0',
   // Version of the design-token contract (see tokens.json / TOKENS.md).
   // Bump on any token add/remove/rename so consumers can assert compatibility.
   tokensVersion: '1.2.0',
